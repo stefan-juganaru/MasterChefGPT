a559f68dfad0c693b7bfc2fa9ad57d68da29d5b2
Stefan Juganaru <stefan.juganaru1612@gmail.com>
2024-02-06 12:12:26 +0200

    Landing page revamp

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import {redirect} from "next/navigation";
 import {currentUser} from "@clerk/nextjs";
+import chef from "@/assets/chef.svg"
+import Image from "next/image"
 
 const Home =  async () => {
     const user = await currentUser();
@@ -11,10 +13,11 @@ const Home =  async () => {
   return (
       <div className="hero min-h-screen bg-base-200 overflow-y-hidden">
           <div className="hero-content flex-col lg:flex-row-reverse">
-              <p>IMAGE</p>
+              <Image src={chef} alt="landing chef"/>
               <div>
                   <h1 className="text-6xl font-bold">MasterChefGPT</h1>
-                  <p className="py-6 max-w-2xl leading-loose">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                  <p className="py-6 max-w-2xl leading-loose">Discover the joy of cooking with our AI Chef, your personal culinary assistant. Tailored to your preferences and skill level,
+                      it offers guided recipes and meal suggestions based on ingredients you already have.</p>
                   <Link href="/sign-in" className="btn btn-accent">Get Started</Link>
               </div>
           </div>
