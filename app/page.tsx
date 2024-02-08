f39a2195621657a04452c6b29a851ea28f8ccd5f
Stefan Juganaru <stefan.juganaru1612@gmail.com>
2024-02-08 13:18:31 +0200

    Fixed landing page. Made it responsive

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,14 @@ const Home =  async () => {
     }
   return (
       <div className="hero min-h-screen bg-base-200 overflow-y-hidden">
-          <div className="hero-content flex-col lg:flex-row-reverse">
-              <Image src={chef} alt="landing chef"/>
+          <div className="hero-content grid-cols-[1fr,auto]">
               <div>
-                  <h1 className="text-6xl font-bold">MasterChefGPT</h1>
-                  <p className="py-6 max-w-2xl leading-loose">Discover the joy of cooking with our AI Chef, your personal culinary assistant. Tailored to your preferences and skill level,
+                  <h1 className="text-4xl font-bold lg:text-6xl">MasterChefGPT</h1>
+                  <p className="py-6 max-w-xl leading-loose">Discover the joy of cooking with our AI Chef, your personal culinary assistant. Tailored to your preferences and skill level,
                       it offers guided recipes and meal suggestions based on ingredients you already have.</p>
                   <Link href="/sign-in" className="btn btn-accent">Get Started</Link>
               </div>
+              <Image src={chef} width={525} height={525} alt="landing chef" className="hidden lg:block"/>
           </div>
       </div>
   );
